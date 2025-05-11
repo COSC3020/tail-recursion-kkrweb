51169b473395dd126128ca109105120d8099836f
Kane Kriz <kanemkriz@gmail.com>
2025-05-11 11:51:55 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,46 @@
+// Kane Kriz
+// UWYO COSC 3020 Algorithms - tail recursion primary js file
+// 11 May 2025
 //
 //
 //
+
+
+
 //
-//
+
+
+
+//tail recursive fib
+function fib(n)
+{
+    function fibHelper(currentIteration, fibArr)
+    {
+        if(currentIteration > n)
+        {
+            return fibArr;
+        }
+
+        if(currentIteration == 0) 
+        {
+            fibArr.push(0);
+            return fibHelper(currentIteration + 1, fibArr);
+        } 
+          
+        else if(currentIteration == 1) 
+        {
+            fibArr.push(1);
+            return fibHelper(currentIteration + 1, fibArr);
+        } 
+          
+        else 
+        {
+            fibArr.push(fibArr[currentIteration - 1] + fibArr[currentIteration - 2]);
+            return fibHelper(currentIteration + 1, fibArr);
+        }
+    }
+
+    return fibHelper(0, []);
+}
+
 //
